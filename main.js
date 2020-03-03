84b74e856c6ebd9df2a5eb9b940f1367a118834b
Ben Guzik <benjamin.guzik@uconn.edu>
2020-03-03 14:34:22 -0500

    More Service Worker Updates

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-// Register a service worker, this one located in serviceworker.js
-// A service worker is a piece of code the browser runs behind the scenes.
-if ('serviceWorker' in navigator) {
-  console.log('CLIENT: service worker registration in progress.');
-  navigator.serviceWorker.register('sw.js').then(function() {
-    console.log('CLIENT: service worker registration complete.');
-  }, function() {
-    console.log('CLIENT: service worker registration failure.');
-  });
-} else {
-  console.log('CLIENT: service workers are not supported.');
-}
+
 
 var sampleMessage = {
   sentBy:"Ben",
